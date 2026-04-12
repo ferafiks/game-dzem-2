3a5f81e63f2fe3736a435fc2b095a57194f967a8
Zapzap <zuzannaa.pietrzak@gmail.com>
2026-04-12 10:28:15 +0200

    main menu buttons connected

---
+++ mm.gd
@@ -0,0 +1,15 @@
+extends Control
+var lvl = preload("res://test_level.tscn")
+var credits = preload("res://credits.tscn")
+
+func _on_quit() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func on_credits() -> void:
+	get_tree().change_scene_to_packed(credits)
+
+
+func _on_start() -> void:
+	get_tree().change_scene_to_packed(lvl)
